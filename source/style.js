5d93a9d9eccdf791408f0b0dad12a380d7194aa5
Giovanni Calò <giovanni.calo@live.it>
2020-08-19 06:47:21 +0100

    Added style

---
+++ source/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Element = styled.div`
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	flex-grow: 0;
+	flex-shrink: 0;
+	position: relative;
+`;
+
+export default Element;
