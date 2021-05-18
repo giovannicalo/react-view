9393235191b4b57be1e487ea1d1f20eaa9a11ec5
Giovanni Calò <giovanni.calo@live.it>
2021-05-18 22:00:46 +0100

    Improved readability

--- source/generate-random-color.js
+++ source/generate-random-color.js
@@ -3,11 +3,12 @@ const colors = new Set();
 const generateRandomColor = () => {
 	let color = null;
 	while (!color || colors.has(color)) {
-		color = `#${`00000${(Math.random() * 0xFFFFFF | 0).toString(16)}`.slice(-6).toUpperCase()}`;
+		color = `#${`00000${
+			Math.trunc(Math.random() * 0xFFFFFF).toString(16)
+		}`.slice(-6).toUpperCase()}`;
 	}
 	colors.add(color);
 	return color;
 };
 
 export default generateRandomColor;
-
