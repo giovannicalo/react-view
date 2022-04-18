b33cae90a7a50c0a20f1b9e7a59eb3876be999ca
Giovanni Calò <giovanni.calo@live.it>
2022-04-18 12:25:14 +0100

    Added JSDoc

--- source/index.d.ts
+++ source/index.d.ts
@@ -4,4 +4,11 @@ interface ViewProps extends AllHTMLAttributes<HTMLElement> {
 	element?: keyof JSX.IntrinsicElements | JSXElementConstructor<any>;
 }
 
+/**
+ * A generic React component that renders an HTML element with sensible default styles.
+ *
+ * @param {ViewProps} props - React props.
+ * @param {String} [props.element=div] - The HTML element to render, defaults to `div`.
+ * @returns {ReactElement} A React element.
+ */
 export default function View(props: ViewProps): ReactElement<any, any>
