bc5825bc522020174c42b0b1dca18ee3dd717fbf
Giovanni Calò <giovanni.calo@live.it>
2022-04-18 14:29:57 +0100

    Removed unnecessary wrapper

--- source/index.js
+++ source/index.js
@@ -1,9 +1,18 @@
-import { forwardRef } from "react";
+import { forwardRef, useMemo } from "react";
 
-import Component from "./component";
+import generateDebugStyle from "./generate-debug-style";
+import Element from "./style";
 
-const View = forwardRef((props, ref) => {
-	return <Component {...props} innerRef={ref} />;
+const View = forwardRef(({ children, element, style, ...props }, ref) => {
+	const debugStyle = useMemo(generateDebugStyle, []);
+	return <Element
+		{...props}
+		as={element}
+		ref={ref}
+		style={{ ...style, ...debugStyle }}
+	>
+		{children}
+	</Element>;
 });
 
 export default View;
--- source/index.spec.js
+++ source/index.spec.js
@@ -1,15 +1,50 @@
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { mount, render, shallow } from "enzyme";
 import { createRef } from "react";
 
 import View from ".";
-import Component from "./component";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 it("should propagate its ref to the inner component", () => {
 	const ref = createRef();
-	expect(shallow(
-		<View ref={ref} />
-	).find(Component).prop("innerRef")).toBe(ref);
+	mount(<View ref={ref} />);
+	expect(ref.current).toBeTruthy();
+});
+
+it("should create a DIV element by default", () => {
+	expect(render(<View />).is("div")).toBeTruthy();
+});
+
+it("should create the given HTML element", () => {
+	expect(render(<View element="span" />).is("span")).toBeTruthy();
+});
+
+it("shouldn't have a background color by default", () => {
+	expect(
+		shallow(<View />).prop("style")
+	).not.toHaveProperty("backgroundColor");
+});
+
+it("should have a random background color when the debug flag is set", () => {
+	global.debugViews = true;
+	expect(
+		shallow(<View />).prop("style")
+	).toHaveProperty("backgroundColor");
+	global.debugViews = false;
+});
+
+it("should propagate its props to the inner element", () => {
+	const component = shallow(<View
+		className="foo"
+		style={{ color: "#000000" }}
+	/>);
+	expect(component.prop("className")).toBe("foo");
+	expect(component.prop("style")).toHaveProperty("color", "#000000");
+});
+
+it("should render the given children", () => {
+	expect(shallow(<View>
+		<span />
+	</View>).contains(<span />)).toBeTruthy();
 });
