4f34cb3ab54c11a87f04a386d81d0c4e14f29777
Giovanni Calò <giovanni.calo@live.it>
2022-09-21 04:04:50 +0100

    Improved types

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,14 +1,14 @@
-import { AllHTMLAttributes, JSXElementConstructor, ReactElement } from "react";
+import { AllHTMLAttributes, ComponentType, ReactElement } from "react";
 
 interface ViewProps extends AllHTMLAttributes<HTMLElement> {
-	element?: keyof JSX.IntrinsicElements | JSXElementConstructor<any>;
+	element?: keyof JSX.IntrinsicElements | ComponentType;
 }
 
 /**
  * A generic React component that renders an HTML element with sensible default styles.
  *
  * @param {ViewProps} props - React props.
- * @param {String} [props.element=div] - The HTML element to render, defaults to `div`.
+ * @param {ComponentType|String} [props.element=div] - The HTML element or React Component to render, defaults to `div`.
  * @returns {ReactElement} A React element.
  */
-export default function View(props: ViewProps): ReactElement<any, any>
+export default function View(props: ViewProps): ReactElement;
