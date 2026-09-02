df033a5ecfae65ad4c5bedb926bc1ca980aee408
Giovanni Calò <giovanni.calo@live.it>
2026-09-02 07:35:16 +0100

    Updated Jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { defineConfig } = require("jest");
+
+module.exports = defineConfig({
 	collectCoverage: true,
 	collectCoverageFrom: ["source/**/*.js"],
 	testEnvironment: "jsdom"
-};
+});
