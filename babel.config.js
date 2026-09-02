7e717087fe0dbb1ade72517664d5e32ce6ffd85c
Giovanni Calò <giovanni.calo@live.it>
2026-09-02 09:30:12 +0100

    Removed build step

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-	presets: [["@babel/env", {
-		bugfixes: true,
-		shippedProposals: true,
-		targets: { node: "26.8.1" }
-	}], ["@babel/react", {
-		runtime: "automatic"
-	}]]
+	presets: ["@babel/env", "@babel/react"],
+	targets: { node: "current" }
 };
